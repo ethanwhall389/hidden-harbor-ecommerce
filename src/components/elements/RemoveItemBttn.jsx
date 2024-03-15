5dac21a1a978c7b3cec1f4aa787849ee4347f666
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2024-03-15 20:45:37 +0000

    Style remove from cart button

--- src/components/elements/RemoveItemBttn.jsx
+++ src/components/elements/RemoveItemBttn.jsx
@@ -8,7 +8,7 @@ export default function RemoveItemBttn({ cart, setCart, productData }) {
 
   return (
     <button
-      className="font-mont text-lg w-40 border-2 border-slate-400 py-2 px-4 mt-2 rounded-full transition-all duration-75"
+      className="font-mont text-lg w-40 border-2 border-slate-400 py-2 px-4 mt-2 rounded-full transition-all duration-75 text-slate-950 hover:border-red-300"
       onClick={handleClick}
     >
       Remove Item
